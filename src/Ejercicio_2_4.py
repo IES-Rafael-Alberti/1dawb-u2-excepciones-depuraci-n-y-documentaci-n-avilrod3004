51348b40f4870a5d28dc5052f9bfd756901ceb2c
avilrod3004 <avilrod3004@g.educaand.es>
2023-11-09 16:40:39 +0100

    Terminado (no tiene test)

---
+++ src/Ejercicio_2_4.py
@@ -0,0 +1,25 @@
+'''
+El algoritmo burbuja
+
+El algoritmo burbuja te permite ordenar valores de un array. Funciona revisando cada elemento con el elemanto adyacente. Si ambos elementos no están ordenados, se procede a intercambiarlos, si por el contrario los elementos ya estaban ordenados se dejan tal como estaban. Este proceso sigue para cada elemento del arreglo hasta que quede completamente ordenado.
+'''
+
+def main():
+    a = [8, 3, 1, 19, 14]
+
+    n = len(a)
+
+    for i in range(0, (n - 1)):
+        for j in range(0, (n - 1 - i)):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+            
+            j += 1
+        
+        i += 1
+
+    print(a)
+
+
+if __name__ == "__main__":
+    main()
